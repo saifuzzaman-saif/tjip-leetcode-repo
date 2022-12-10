a7a15f65fd746bb0ea90044f4924d19888269c40
Saif-IIT-JU <57219416+Saif-IIT-JU@users.noreply.github.com>
2022-12-10 14:05:52 +0600

    Added the solution of Largest Number problem.

---
+++ week02/LargestNumber.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    // TC = O(n Log(n))
+    // MC = O(1)
+
+    public String largestNumber(int[] nums) {
+        List<String>numStrings = new ArrayList<>();
+
+        for(int num : nums) {
+            numStrings.add(String.valueOf(num));
+        }
+
+        Collections.sort(numStrings, (s1, s2) -> (s2 + s1).compareTo(s1 + s2));
+
+        if (numStrings.get(0).equals("0")) {
+            return "0";
+        }
+        
+        StringBuffer largestVal = new StringBuffer();
+
+        for (String s : numStrings) {
+            largestVal.append(s);
+        }
+
+        return new String(largestVal);
+    }
+}
