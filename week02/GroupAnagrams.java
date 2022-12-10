56592d79c4324a7ddddf3f78c5d79a120df8d8dd
Saif-IIT-JU <57219416+Saif-IIT-JU@users.noreply.github.com>
2022-12-10 13:33:49 +0600

    Solved Group Anagrams problem.

---
+++ week02/GroupAnagrams.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    // TC = O(NK) ; Where N is the number of strings & K is the length of each string;
+    // MC = O(N)
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<Integer, List<String>>anagrams = new HashMap<>();
+
+        for (String str : strs) {
+            int hashVal = getHashVal(str);
+            List<String>list = anagrams.getOrDefault(hashVal, new ArrayList<String>());
+            list.add(str);
+            anagrams.put(hashVal, list);
+        }
+
+        return new ArrayList<>(anagrams.values());
+    }
+
+    private int getHashVal(String s) {
+        long hash = 1, offset = 997, mod = 1_000_000_007;
+
+        for (int i = 0; i < s.length(); i++) {
+            int ch = s.charAt(i) - 'a';
+            hash = (hash * (ch + offset)) % mod;
+        }
+
+        return (int) hash;
+    }
+}
