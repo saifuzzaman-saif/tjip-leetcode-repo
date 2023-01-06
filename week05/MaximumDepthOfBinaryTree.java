2c39078cc5cc1c667c49e41dd48d71f4469697e3
Saif-IIT-JU <57219416+Saif-IIT-JU@users.noreply.github.com>
2023-01-06 12:05:41 +0600

    Added the solution of Maximum Depth of Binary Tree problem.

---
+++ week05/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+// TC = O(n); where n is number of nodes in the tree.
+// MC = O(n) in worst case(if the three is fully imbalanced).
+// Problem link : https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+
+class Solution {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+
+        return 1 + Math.max(leftDepth, rightDepth);
+    }
+}
