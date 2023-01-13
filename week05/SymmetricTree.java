d79ca206fd371b09ca5f4933bc8b36d6d769cac4
saifuzzaman <saifuriit46@gmail.com>
2023-01-13 23:37:59 +0600

    Solved Symmetric Tree problem.

---
+++ week05/SymmetricTree.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+// TC = O(n); where n is the number of nodes;
+// MC = O(H); where H is the height of the tree, required for recursion.
+// Problem link : https://leetcode.com/problems/symmetric-tree/description/
+
+class Solution {
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        return isMirror(root.left, root.right);
+    }
+
+    private boolean isMirror(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+
+        if (left == null || right == null) {
+            return false;
+        }
+
+        if (left.val != right.val) {
+            return false;
+        }
+
+        return isMirror(left.left, right.right) && isMirror(left.right, right.left);
+    }
+}
