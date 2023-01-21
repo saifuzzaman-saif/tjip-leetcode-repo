3e0d0168794152e79047b85dd101e2d8f0aeab8a
saifuzzaman <saifuriit46@gmail.com>
2023-01-22 01:02:23 +0600

    Solved Word Break problem.

---
+++ week06/WordBreak.java
@@ -0,0 +1,80 @@
+/**
+* TC = O(M + (N * L)); where M is total number of characters in words, 
+*                      N is the length of string s & L is max length of word;
+* MC = O(M + N);
+* Problem link : https://leetcode.com/problems/word-break/description/
+*/
+
+class Solution {
+
+    private static final int LMT = 26;
+    private static final int mxWordLen = 20;
+    private TrieNode root = new TrieNode();
+    private Boolean[] dp;
+    private String s;
+
+    class TrieNode {
+        boolean isEnd;
+        TrieNode[] next;
+
+        TrieNode() {
+            isEnd = false;
+            next = new TrieNode[LMT];
+        }
+    }
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        for (String word : wordDict) {
+            insert(word);
+        }
+
+        dp = new Boolean[s.length()];
+        this.s = s;
+
+        return recur(0);
+    }
+
+    private Boolean recur(int idx) {
+        if (idx == s.length()) {
+            return true;
+        }
+
+        if (dp[idx] != null) {
+            return dp[idx];
+        }
+
+        TrieNode curr = root;
+        Boolean isPossible = false;
+
+        for (int i = 0; i < mxWordLen && i + idx < s.length(); i++) {
+            int ch = s.charAt(idx + i) - 'a';
+            if (curr.next[ch] == null) {
+                break;
+            }
+
+            curr = curr.next[ch];
+
+            if (curr.isEnd) {
+                isPossible |= recur(idx + i + 1);
+            }
+        }
+
+        return dp[idx] = isPossible;
+    }
+
+    private void insert(String word) {
+        TrieNode curr = root;
+
+        for (char c : word.toCharArray()) {
+            int idx = c - 'a';
+
+            if (curr.next[idx] == null) {
+                curr.next[idx] = new TrieNode();
+            }
+
+            curr = curr.next[idx];
+        }
+
+        curr.isEnd = true;
+    }
+}
